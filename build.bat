6fab6004e37b67256ff67b88a8ea4f88242e60c3
unknown <joebogner@gmail.com>
2015-07-07 17:53:49 -0400

    add options to build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
+@if "%1" == "debug" GOTO debug
 %windir%\microsoft.net\framework64\v4.0.30319\csc.exe /o /out:microj.exe microj.cs
+@GOTO end
+
+:DEBUG
+%windir%\microsoft.net\framework64\v4.0.30319\csc.exe /debug /out:microj.exe microj.cs
+@GOTO end
+
+:END
 
