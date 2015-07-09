5625284b96f7d46b73a688f985189fef9dd9eec1
unknown <joebogner@gmail.com>
2015-07-09 09:17:34 -0400

    comment changes

--- microj.cs
+++ microj.cs
@@ -368,7 +368,7 @@ namespace MicroJ
             var verb = ((A<Verb>) method).Ravel[0];
 
             if (verb.conj == "\"") {
-                //todo: add special code for +/"n or use some type of integrated rank support
+                //future: add special code for +/"n or use some type of integrated rank support
                 if (y.GetType() == typeof(A<long>)) { return rank1ex(method, (A<long>)y); }
                 //todo: evaluate performance of dynamic dispatch of rank -- probably ok
                 else return Verbs.InvokeExpression("rank1ex", method, y,1, this);
